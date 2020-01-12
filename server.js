d1d5ae8c94566451018ed1cdab5ba0fbb237f6ff
Suresh <suresh@tricog.com>
2020-01-12 22:55:08 +0530

    added pino logger

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const express = require('express');
 // Constants
 const PORT = 80;
 const HOST = '0.0.0.0';
+const logger = require('pino')()
 
+logger.info('sample log from pino')
 // App
 const app = express();
 app.get('/', (req, res) => {
@@ -14,5 +16,17 @@ app.get('/', (req, res) => {
   res.send('Hello world\n');
 });
 
+app.get('/test-pino', (req, res) => {
+  logger.trace(`trace`);
+  logger.debug(`debug`);
+  logger.info(`info`);
+  logger.warn(`warn`);
+  logger.error(`error`);
+  logger.fatal(`fatal`);
+
+  res.send('test-pino\n');
+});
+
 app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+logger.info(`Running on http://${HOST}:${PORT}`);
+
